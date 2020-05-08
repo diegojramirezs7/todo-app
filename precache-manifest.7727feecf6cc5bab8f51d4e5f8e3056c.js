ad1afba1eb7fa8cbbb2c3470917ad987c33f4b2e
Diego Ramirez <diegojramirezs7@gmail.com>
2020-05-08 15:20:08 -0700

    Updates

---
+++ precache-manifest.7727feecf6cc5bab8f51d4e5f8e3056c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a61bb362048b15968f51ba597321d3c",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "4a74fd7755a3e53bf5b8",
+    "url": "/todo-app/static/css/main.8a3d7ede.chunk.css"
+  },
+  {
+    "revision": "44b9eb92d603bd407488",
+    "url": "/todo-app/static/js/2.074f12b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.074f12b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a74fd7755a3e53bf5b8",
+    "url": "/todo-app/static/js/main.d73d4ecc.chunk.js"
+  },
+  {
+    "revision": "e3ab739fbe0473f87945",
+    "url": "/todo-app/static/js/runtime-main.2ae80ddd.js"
+  }
+]);
